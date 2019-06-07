97206d5f100b8563a312ced0628448ddc0aba08e
UlianaTret <50988744+UlianaTret@users.noreply.github.com>
2019-06-07 22:55:09 +0300

    ссылки

--- enterUser.php
+++ enterUser.php
@@ -6,7 +6,7 @@ if (isset($PERSON['press'])) {
     // ПРОВЕРКА ПУСТЫХ ПОЛЕЙ
     $empty = '';
     if (($PERSON['login'] != $empty) && ($PERSON['password']!= $empty)) {
-        $mysqli = new mysqli ("127.0.0.1:3307", "root", "7ptyn5VT", "bd0");
+        $mysqli = new mysqli ("84.201.189.23", "tret", "q456123", "bd0");
         $mysqli ->query ("SET NAMES 'utf8'");
         if ($mysqli-> connect_error) {
             die ("Ошибка подключения:". $mysqli-> connect_error);
@@ -72,15 +72,15 @@ if (isset($PERSON['press'])) {
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="http://topilin.ru/uliana/index.php">Parking</a>
+            <a class="navbar-brand" href="http://84.201.189.23/uliana/index.php">Parking</a>
         </div>
         <div class="navbar-collapse collapse">
             <div>
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="#">главная</a></li>
                     <li><a href="#">о нас</a></li>
-                    <li><a href="http://topilin.ru/uliana/user.php">хотите поставить машину?</a></li>
-                    <li><a href="http://topilin.ru/uliana/enterAdmin.php">вы сотрудник?</a></li>
+                    <li><a href="http://84.201.189.23/uliana/user.php">хотите поставить машину?</a></li>
+                    <li><a href="http://84.201.189.23/uliana/enterAdmin.php">вы сотрудник?</a></li>
                 </ul>
             </div>
         </div>
@@ -94,7 +94,7 @@ if (isset($PERSON['press'])) {
 <div id="NoPass" class='alert alert-danger' role='alert'>Неверный пароль</div>
 <div id="lol" class='alert alert-warning' role='alert'>Ожидайте...</div>
 
-<form action="http://topilin.ru/uliana/enterUser.php" class="form-group" method="POST">
+<form action="http://84.201.189.23/uliana/enterUser.php" class="form-group" method="POST">
     <div class="form-group">
         <label for="exampleInputEmail1">Ваше имя</label>
         <input type="text" class="form-control" id="login" name="login" placeholder="Логин">
@@ -143,7 +143,7 @@ if (isset($PERSON['press'])) {
                             $(data).show();
                             break;
                         default:
-                            window.location="http://topilin.ru/uliana/user.php";
+                            window.location="http://84.201.189.23/uliana/user.php";
                             break;
                     }
                 }
